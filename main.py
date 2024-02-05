bb55e51234922187a28e850551ca81350c9534c5
Nimrod katzenell‬‏ <131244030+grumpet@users.noreply.github.com>
2024-02-05 18:31:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+#creat a farmstack fastapi app
+from fastapi import FastAPI , HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi import HTTPException
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+from pymongo import MongoClient
+
+app = FastAPI()
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://admin:asdfqwer1234@map.xkbb8us.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
